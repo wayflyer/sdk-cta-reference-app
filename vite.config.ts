510dfb36ab31b8b02c8c615d7546ac27f85de18e
Iain Maitland <iain.maitland@wayflyer.com>
2025-07-21 13:37:17 +0000

    Add a bundle analyzer

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { analyzer } from "vite-bundle-analyzer";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), analyzer()],
   server: {
     host: "0.0.0.0",
     port: 5173,
